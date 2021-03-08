102465c5c6d0bb9e95c758a61c16428680431ded
farahmustafa <farahmustafa998@gmail.com>
2021-03-08 22:00:06 +0200

    adding background and lists

---
+++ style/javascript/script.js
@@ -0,0 +1,18 @@
+
+ function showform(){
+   document.getElementById("add-list").style.display="block";
+   
+ }
+ function showform1(){
+  document.getElementById("add-card").style.display="block";
+}
+function showform2(){
+  document.getElementById("add-card1").style.display="block";
+}
+function showform3(){
+  document.getElementById("add-card2").style.display="block";
+}
+function showform4(){
+  document.getElementById("add-card3").style.display="block";
+}
+
