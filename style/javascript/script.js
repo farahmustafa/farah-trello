09b58483473c40e15a213764a24b4a360f2459d6
farahmustafa <farahmustafa998@gmail.com>
2021-03-16 20:49:39 +0200

    adding button to create new title card and when you press in the card pop up modal appears

--- style/javascript/script.js
+++ style/javascript/script.js
@@ -1,27 +1,50 @@
-function showform(){
-  document.getElementById("list").style.display="none";
-   document.getElementById("add-list").style.display="block"; 
- }
- function cancelform(){
-  document.getElementById("list").style.display="block";
-  document.getElementById("add-list").style.display="none"; 
+function showform() {
+  document.getElementById("list").style.display = "none";
+  document.getElementById("add-list").style.display = "block";
 }
-function showform4(){
-  document.getElementById("hide3").style.display="none";
-  document.getElementById("add-card3").style.display="block";
+function cancelform() {
+  document.getElementById("list").style.display = "block";
+  document.getElementById("add-list").style.display = "none";
 }
-function cancelform1(){
-  document.getElementById("hide3").style.display="block";
-  document.getElementById("add-card3").style.display="none";   
+function showform1(){
+  document.getElementById("hide3").style.display = "none";
+  document.getElementById("card-title").style.display = "block";
 }
-
+function cancelform1() {
+  document.getElementById("hide3").style.display = "block";
+  document.getElementById("card-title").style.display = "none";
+}
+function myFunction(){
+  document.getElementById("inpt11").style.display = "block"; 
+  document.getElementById("inpp").style.padding=0;
+  document.getElementById("inpp").style.width="80%";
+}
 function add(){
   var lI = document.createElement("li");
-  lI.style.background="white";
-  lI.style.width="268px";
-  lI.style.listStyleType="none";
-  var input = document.getElementById("inpt11").Value;
+  lI.style.background = "white";
+  lI.style.width = "260px";
+  lI.style.listStyleType = "none";
+  lI.style.marginBottom = "3px";
+  lI.style.border = "1px solid";
+  lI.style.padding = "3px";
+  lI.style.position = "relative";
+  lI.style.top = "18px";
+  lI.style.overflow = "auto";
+  var input = document.getElementById("inpp").value;
   var text = document.createTextNode(input);
   lI.appendChild(text);
   document.getElementById("myList").appendChild(lI);
+ document.getElementById("head").innerHTML=input;
+  document.getElementById("inpp").value = "";
+  document.getElementById("card-title").style.display="none";
+  document.getElementById("hide3").style.display="block";
+  lI.setAttribute("data-toggle","modal");
+  lI.setAttribute("data-target","#myModal");
+  var parag =document.getElementById("inpt11").value;
+  document.getElementById("descrip").innerHTML=parag;
 }
+
+
+
+
+
